0d7c8b8f1f32b78ad5559578e5fc924735b0f20d
Jason <jashawoo@gmail.com>
2020-10-27 10:30:37 -0500

    Initial projet creation

---
+++ WebApplication1/About.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="About" Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="About.aspx.cs" Inherits="WebApplication1.About" %>
+
+<asp:Content ID="AboutContent" ContentPlaceholderID="MainContent" runat="server">
+    <asp:Label runat="server" ID="TestLabel2"></asp:Label>
+    <asp:UpdatePanel runat="server" ID="UpdatePanel1">
+        <ContentTemplate>
+            <asp:Timer runat="server" id="UpdateTimer" interval="1000" ontick="UpdateTimer_Tick" />
+            <asp:Label runat="server" ID="TestLabel"></asp:Label>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
---
+++ WebApplication1/About.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                Session["Number"] = 0;
+            }
+        }
+
+        protected void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            int num = (int)Session["Number"];
+            ++num;
+            TestLabel.Text = "Test: " + num.ToString();
+            Session["Number"] = num;
+        }
+    }
+}
